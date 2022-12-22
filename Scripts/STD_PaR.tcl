d91e3ffa951f8b77dbfc043558b0d9b1b80e3952
CitRus754 <71969896+CitRus754@users.noreply.github.com>
2022-12-22 14:17:49 +0300

    Update STD_PaR.tcl

--- Scripts/STD_PaR.tcl
+++ Scripts/STD_PaR.tcl
@@ -28,9 +28,7 @@ floorPlan -coreMarginsBy die -site core_hd -s 300 300 20 20 20 20
 clearGlobalNets
 globalNetConnect VDD -type pgpin -pin vdd -inst * -module {}
 globalNetConnect VDD -type tiehi -pin vdd -inst * -module {}
-globalNetConnect VDD -type tielo -pin vdd -inst * -module {}
 globalNetConnect VSS -type tielo -pin gnd -inst * -module {}
-globalNetConnect VSS -type tiehi -pin gnd -inst * -module {}
 globalNetConnect VSS -type pgpin -pin gnd -inst * -module {}
 addRing -skip_via_on_wire_shape Noshape -skip_via_on_pin Standardcell -stacked_via_top_layer METTPL -type core_rings -jog_distance 2.8 -threshold 2.8 -nets {VDD VSS} -follow core -stacked_via_bottom_layer MET1 -layer {bottom MET1 top MET1 right MET2 left MET2} -width 3 -spacing 0.6 -offset 3.15
 addStripe -skip_via_on_wire_shape Noshape -block_ring_top_layer_limit MET3 -max_same_layer_jog_length 6 -padcore_ring_bottom_layer_limit MET1 -set_to_set_distance 60 -skip_via_on_pin Standardcell -stacked_via_top_layer METTPL -padcore_ring_top_layer_limit MET3 -spacing 2.5 -merge_stripes_value 2.8 -layer MET2 -block_ring_bottom_layer_limit MET1 -width 3 -nets {VSS VDD} -stacked_via_bottom_layer MET1
@@ -42,19 +40,15 @@ deleteSelectedFromFPlan
 
 sroute -connect { blockPin padPin padRing corePin floatingStripe } -layerChangeRange { MET1 METTPL } -blockPinTarget { nearestTarget } -padPinPortConnect { allPort oneGeom } -padPinTarget { nearestTarget } -corePinTarget { firstAfterRowEnd } -floatingStripeTarget { blockring padring ring stripe ringpin blockpin followpin } -allowJogging 1 -crossoverViaLayerRange { MET1 METTPL } -nets { VDD VSS } -allowLayerChange 1 -blockPin useLef -targetViaLayerRange { MET1 METTPL }
 editPowerVia -skip_via_on_pin Standardcell -bottom_layer MET1 -add_vias 1 -top_layer METTPL
-set sprEpvLayers {}
 
 #-------------------------Placement----------------------------------#
 
-redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 timeDesign -prePlace -idealClock -pathReports -drvReports -slackReports -numPaths 50 -prefix STD_prePlace -outDir ../Reports/Layout/Timing_prePlace
 setPlaceMode -fp false
 placeDesign -inPlaceOpt
 
 #---------------------Clock-Tree Synthesis--------------------------------#
-redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 timeDesign -preCTS -idealClock -pathReports -drvReports -slackReports -numPaths 50 -prefix STD_preCTS_s -outDir ../Reports/Layout/Timing_preCTS_setup
-redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 timeDesign -preCTS -hold -idealClock -pathReports -slackReports -numPaths 50 -prefix STD_preCTS_h -outDir ../Reports/Layout/Timing_preCTS_hold
 setOptMode -fixCap true -fixTran true -fixFanoutLoad true
 optDesign -preCTS
@@ -63,9 +57,7 @@ setCTSMode -engine ck
 clockDesign -specFile Clock.ctstch -outDir clock_report -fixedInstBeforeCTS
 
 #-------------------------Routing---------------------------------#
-redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 timeDesign -postCTS -pathReports -drvReports -slackReports -numPaths 50 -prefix STD_postCTS_s -outDir ../Reports/Layout/Timing_postCTS_setup
-redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 timeDesign -postCTS -hold -pathReports -slackReports -numPaths 50 -prefix STD_postCTS_h -outDir ../Reports/Layout/Timing_postCTS_hold
 setOptMode -fixCap true -fixTran true -fixFanoutLoad true
 optDesign -postCTS
@@ -81,9 +73,7 @@ routeDesign -globalDetail
 
 #--------------------------Final Steps and Verify---------------------------------#
 setAnalysisMode -analysisType onChipVariation -skew true -clockPropagation sdcControl
-redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 timeDesign -postRoute -pathReports -drvReports -slackReports -numPaths 50 -prefix STD_postRoute_s -outDir ../Reports/Layout/Timing_postRoute_setup
-redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 timeDesign -postRoute -hold -pathReports -slackReports -numPaths 50 -prefix STD_postRoute_h -outDir ../Reports/Layout/Timing_postRoute_hold
 setOptMode -fixCap true -fixTran true -fixFanoutLoad true
 optDesign -postRoute
@@ -99,16 +89,12 @@ setExtractRCMode -engine postRoute -effortLevel signoff
 extractRC
 
 #------------------------Sign-Off and Generation Files-----------------------------------#
-redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 timeDesign -signoff -pathReports -drvReports -slackReports -numPaths 50 -prefix STD_signOff_s -outDir ../Reports/Layout/Timing_SignOff_setup
-redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 timeDesign -signoff -hold -pathReports -slackReports -numPaths 50 -prefix STD_signOff_h -outDir ../Reports/Layout/Timing_SignOff_hold
 all_hold_analysis_views 
 all_setup_analysis_views 
 write_sdf -view TYPview ../Outputs/SigTransDetect.sdf
 saveNetlist ../Outputs/SigTransDetect_logic.v
 saveNetlist ../Outputs/SigTransDetect_phys.v -includePhysicalCell {FEED7HD FEED10HD FEED15HD FEED1HD FEED25HD FEED2HD FEED3HD FEED5HD}
-global dbgLefDefOutVersion
-set dbgLefDefOutVersion 5.8
 defOut -floorplan -netlist -routing ../Outputs/SigTransDetect.def
 
